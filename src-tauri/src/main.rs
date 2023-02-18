31f3f70e5819a000e68e7ff766a99d3b6084478d
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-02-19 04:43:21 +0900

    update: 標準のタイトルバーを削除し、閉じるボタンをツールバーの中に入れた

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,16 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Window;
+
+#[tauri::command]
+fn close_window(window: Window) {
+    window.close().unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![close_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,69 @@
-import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
+import { MDEditorProps, PreviewType } from "@uiw/react-md-editor";
+import * as commands from "@uiw/react-md-editor/lib/commands";
+import "@uiw/react-md-editor/markdown-editor.css";
 import dynamic from "next/dynamic";
-import { commands, PreviewType } from "@uiw/react-md-editor";
+import { useState } from "react";
 import * as zoom from "./zoom";
-const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
+const MDEditor = dynamic<MDEditorProps>(() => import("@uiw/react-md-editor"), {
+  ssr: false,
+});
+
+const closeWindow = {
+  name: "close-window",
+  keyCommand: "closeWindow",
+  buttonProps: { "aria-label": "Close window" },
+  icon: <span id="titlebar-close">✕</span>,
+  execute: () => {
+    if (process.browser) {
+      invoke("close_window");
+    }
+  },
+};
 
 function App() {
   const [value, setValue] = useState("**Hello world!!!**");
-  const [preview, setPreview] = useState<PreviewType>("live");
+  const [preview, setPreview] = useState<PreviewType>("edit");
   const [hiddenToolbar, setHiddenToolbar] = useState(false);
 
   if (process.browser) {
     window.onblur = () => {
-      setHiddenToolbar(true);
       setPreview("preview");
     };
 
     window.onfocus = () => {
-      setHiddenToolbar(false);
       setPreview("edit");
     };
+
+    window.addEventListener("mouseover", () => {
+      setHiddenToolbar(false);
+
+      // ツールバーをドラッグ可能にする
+      let toolbar = document.querySelector(".w-md-editor-toolbar");
+      toolbar?.setAttribute("data-tauri-drag-region", "");
+    });
+
+    window.addEventListener("mouseout", () => {
+      setHiddenToolbar(true);
+    });
   }
 
   return (
-    <div>
+    <div className="container">
       <MDEditor
         value={value}
         onChange={setValue}
         fullscreen={true}
         preview={preview}
         hideToolbar={hiddenToolbar}
-        // commands で欲しいコマンドを指定するとエラーになるため、フィルターで除外する
-        // なぜか list 系のコマンドは除外できない
-        commandsFilter={(cmd) => {
-          if (
-            /(bold|italic|title|link|quote|code|codeblock|image|divider|unorderedListCommand|orderedListCommand|checkedListCommand|fullscreen)/.test(
-              cmd.name
-            )
-          ) {
-            return false;
-          }
-          return cmd;
-        }}
+        commands={[commands.comment, commands.strikethrough, commands.hr]}
+        extraCommands={[
+          commands.codeLive,
+          commands.codeEdit,
+          commands.codePreview,
+          closeWindow,
+        ]}
         onWheel={zoom.handleWheel}
         onKeyDown={zoom.handleKeyDown}
         className="w-md-editor-fullscreen" // 常にフルスクリーンにする
