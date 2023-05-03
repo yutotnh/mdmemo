2f6ffce8264383cd4c11d3fb430efb46c2409f04
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-05-04 05:47:19 +0900

    update: アプリの情報をコピーできるようにした #50

--- src/commands/about.tsx
+++ src/commands/about.tsx
@@ -1,6 +1,6 @@
 import { getName, getTauriVersion, getVersion } from "@tauri-apps/api/app";
 import * as os from "@tauri-apps/api/os";
-import { message } from "@tauri-apps/api/dialog";
+import { confirm } from "@tauri-apps/api/dialog";
 import { ICommand } from "@uiw/react-md-editor";
 
 /**
@@ -25,10 +25,20 @@ export const about: ICommand = {
         version: await os.version(),
       };
 
-      message(
-        `${appName}\nVersion: ${appVersion}\nTauri: ${tauriVersion}\nOS: ${osInfo.type} ${osInfo.arch} ${osInfo.version}`,
-        "About"
-      );
+      const text = `${appName}\nVersion: ${appVersion}\nTauri: ${tauriVersion}\nOS: ${osInfo.type} ${osInfo.arch} ${osInfo.version}`;
+
+      // 以下のようなダイアログにしたいので、標準のダイアログの配置から変更する(Ok/Cancelのボタンを入れ替える)
+      // - 左のボタンを押したらクリップボードに表示内容をコピーする
+      //   - コピーしたらダイアログを閉じる(これは目的ではなくて、ダイアログの仕様)
+      // - 右のボタンを押したらダイアログを閉じる
+      confirm(text, {
+        title: "About",
+        type: "info",
+        okLabel: "Copy",
+        cancelLabel: "OK",
+      }).then(() => {
+        navigator.clipboard.writeText(text);
+      });
     };
 
     printAbout();
