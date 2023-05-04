726ef0818b6cad4506d48fcad8571cae671b7486
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-05-05 08:24:27 +0900

    update: ファイルに保存されていない変更がある場合に確認する処理を追加した #69

---
+++ src/Close.tsx
@@ -0,0 +1,69 @@
+import { appWindow } from "@tauri-apps/api/window";
+import { useEffect } from "react";
+import { confirm } from "@tauri-apps/api/dialog";
+import { invoke } from "@tauri-apps/api";
+
+// リクエストが重複しないように宣言した変数
+// Trueのときは、リクエストが重複している
+let isCloseRequest = false;
+
+/**
+ *
+ * ウィンドウを閉じる時の処理を追加する
+ *
+ * @returns 空のReact要素
+ */
+export function Close() {
+  /**
+   * ウィンドウを閉じる
+   *
+   * 保存されていない変更がある場合は、保存するかどうかを確認する
+   */
+  function closeWindow() {
+    if (isCloseRequest) return;
+
+    isCloseRequest = true;
+
+    invoke("get_path")
+      // Okで帰ってきたときは、ファイルに保存されているので、そのままウィンドウを閉じる
+      .then(() => {
+        appWindow.close();
+      })
+      // Errで帰ってきたときは、ファイルに保存されていないので、保存するかどうかを確認する
+      .catch(() => {
+        confirm("Do you want to close this window without saving changes?", {
+          title: "mdmemo",
+          type: "warning",
+          okLabel: "OK",
+          cancelLabel: "Cancel",
+        })
+          .then((confirmed) => {
+            if (confirmed) {
+              appWindow.close();
+            }
+          })
+          .catch(() => {})
+          .finally(() => {
+            isCloseRequest = false;
+          });
+      });
+  }
+
+  // ウィンドウを閉じるイベントが発火したら、ウィンドウを閉じる処理を実行するようにする
+  useEffect(() => {
+    let unListen = (async () => {
+      const unListen = await appWindow.onCloseRequested((event) => {
+        closeWindow();
+        event.preventDefault();
+      });
+      return unListen;
+    })();
+
+    return () => {
+      (async () => {
+        (await unListen)();
+      })();
+    };
+  }, []);
+  return <></>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import { Close } from "./Close";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
+      <Close />
       <App />
     </RecoilRoot>
   </React.StrictMode>
